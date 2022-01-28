884daebd58a61e9634e9e620eb2260ddee9579a3
Youngjae Lim <limyoungjae@gmail.com>
2022-01-28 15:16:45 -0500

    Added functionality of creating redis client and implemented ResolveURL handler

--- api/go.mod
+++ api/go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/go-redis/redis/v8 v8.11.4
 	github.com/gofiber/fiber/v2 v2.25.0 // indirect
 	github.com/joho/godotenv v1.4.0
 	github.com/klauspost/compress v1.14.2 // indirect
@@ -12,4 +13,9 @@ require (
 	github.com/valyala/fasthttp v1.33.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220111092808-5a964db01320 // indirect
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 )
---
+++ api/routes/resolve.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/youngjae-lim/url-shortener/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	rc := database.CreateRedisClient(0)
+	defer rc.Close()
+
+  value, err :=	rc.Get(database.ctx, url)
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "short not found in the database"})
+	} else if err != nil {
+    return c.Status(fiber.StatusInternalServerError).JSON(fiber.map{"error": "cannot connect to DB"})
+  }
+
+  rIncrement := database.CreateRedisClient(1)
+  defer rIncrement.Close()
+
+  _ = rIncrement.Incr(database.ctx, "counter")
+
+  return c.Redirect(value, 301)
+}
